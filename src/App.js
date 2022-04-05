3a68ef665d4540e10e7eeed85e59facc51d3106f
Joseph <yosephassefa85@gmail.com>
2022-04-05 20:02:42 +0300

    adding date component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import ExpenseItem from './components/ExpenseItem';
 import logo from './logo.svg';
 
 function App() {
-  const expenses=[
+  const expenses = [
     {
-    id:'e1',
-    title:'New Desk ',
-    amount:450,
-    date:new Date(2021,4,4),
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id:'e2',
-      title:'Car insurance ',
-      amount:50,
-      date:new Date(2021,4,4),
-      },
-      {
-        id:'e3',
-        title:'Table ',
-        amount:200,
-        date:new Date(2021,7,4),
-      },
-  
-];
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -41,6 +41,10 @@ function App() {
       title={expenses[2].title} 
       amount={expenses[2].amount}
       date={expenses[2].date}></ExpenseItem>
+              <ExpenseItem 
+      title={expenses[3].title} 
+      amount={expenses[3].amount}
+      date={expenses[3].date}></ExpenseItem>
  
         <a
           className="App-link"
