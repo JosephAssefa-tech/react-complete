aff95967ef1d8472df8006a3ce550b7f35e87e50
Joseph <yosephassefa85@gmail.com>
2022-04-06 17:08:40 +0300

    react hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
 import './ExpenseItem.css';
 
+import React,{useState} from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate  from './ExpenseDate';
 
 const ExpenseItem =(props)=>
 {
+    const [title,updateTitle]=useState(props.title);
+   
 const clickHandeler=()=>{
-    console.log("clicking handler")
+    updateTitle("updateing")
+   
 }
 
     return(
         <Card className="expense-item">
     <ExpenseDate date={props.date}/>
            <div className="expense-item__description">
-               <h2>{props.title}</h2>
+               <h2>{title}</h2>
                <div className="expense-item__price">{props.amount}</div>
            </div>
     <button onClick={clickHandeler}>click button</button>
