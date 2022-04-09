eaf96c501d465b244b7e8cbcf7ffc6152c7e5a37
Joseph <yosephassefa85@gmail.com>
2022-04-09 16:19:24 +0300

    renaming title for id e1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App =()=> {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
+      title: 'Toilet Paper1',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
